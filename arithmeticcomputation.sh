52763fb9a615158e0f003c7287f9fcc6b190a54e
Desale1 <dkdesale08@gmail.com>
2022-12-30 19:53:44 +0530

    Added: taking 3 inputs from user

---
+++ arithmeticcomputation.sh
@@ -0,0 +1,9 @@
+echo "Welcome to Sorting Arithmetic Computation program!!!"
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+read -p "Enter third number: " num3
+echo $num1 $num2 $num3
+
+
+
