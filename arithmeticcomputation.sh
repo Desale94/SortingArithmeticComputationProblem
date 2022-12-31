dbf45e4465c1e1d9c55ac3bb45b5a535d77cc155
Desale1 <dkdesale08@gmail.com>
2022-12-31 15:37:40 +0530

    Added: store the result in dictionary

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -3,6 +3,7 @@ echo "Welcome to Sorting Arithmetic Computation program!!!"
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
 read -p "Enter third number: " num3
+echo "The given three numbers:"
 
 total1=$(($num1+$num2*$num3))
 echo "$num1+$num2*$num3:$total1"
@@ -16,3 +17,12 @@ echo "$num3+$num1/$num2:$total3"
 total4=$(($num1%$num2+$num3))
 echo "$num1%$num2+$num3:$total4"
 
+declare -A dictionary
+dictionary[total1]=$total1
+dictionary[total2]=$total2
+dictionary[total3]=$total3
+dictionary[total4]=$total4
+for computation in ${!dictionary[@]}
+do 
+    echo "$computation = ${dictionary[$computation]}"
+done
