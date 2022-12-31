ba9eb7ce0397fdfa65fc205d31abb18ad60fe911
Desale1 <dkdesale08@gmail.com>
2022-12-31 14:13:25 +0530

    Added: Compute c + a / b

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -7,5 +7,9 @@ read -p "Enter third number: " num3
 total1=$(($num1+$num2*$num3))
 echo "$num1+$num2*$num3:$total1"
 
+total2=$(($num1*$num2+$num3))
+echo "$num1*$num2+$num3:$total2"
 
+total3=$(($num3+$num1/$num2))
+echo "$num3+$num1/$num2:$total3"
 
